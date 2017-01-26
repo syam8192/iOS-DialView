314557b1c08f95ec4812d46f6859530fe49ffa48
yamakami <yamakami_shinobu_gn@cyberagent.co.jp>
2017-01-26 15:55:15 +0900

    サンプル変更

--- DialView/ViewController.swift
+++ DialView/ViewController.swift
@@ -9,8 +9,25 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var dialView: DialView!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let halfWidth = dialView.bounds.width / 2
+        let div = 24
+        for i in 0..<div {
+            let f = CGFloat(i) / CGFloat(div)
+            let label: UILabel = UILabel()
+            label.textColor = UIColor(hue: f, saturation: 1, brightness: 1, alpha: 1)
+            label.text = String(format: "%02d", i)
+            label.sizeToFit()
+            label.center = CGPoint(x: dialView.bounds.midX, y: dialView.bounds.midY)
+            label.transform = CGAffineTransform(rotationAngle: 3.1415927 * 2 * f).translatedBy(x: 0, y: label.bounds.height - halfWidth)
+            dialView.addSubview(label)
+        }
+        dialView.layer.borderColor = UIColor.black.cgColor
+        dialView.layer.borderWidth = 2
+        dialView.layer.cornerRadius = halfWidth
+        dialView.roundedHitRadius = halfWidth
         dialView.delegate = self
     }
 
